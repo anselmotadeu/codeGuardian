9ad81d69069e9b5ddf65673a4d50d0c6f47e0955
anselmotadeu <anselmotadeu@outlook.com>
2024-11-04 08:12:36 -0300

    🚀 feat: add basic routing and controller

--- src/app.js
+++ src/app.js
@@ -4,14 +4,13 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const port = 3000;
+const routes = require('./routes/routes');
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello CodeGuardian!');
-});
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
