81959107a566f3ee6b3b57a2549276b92bfe864b
anselmotadeu <anselmotadeu@outlook.com>
2024-11-04 14:16:40 -0300

    🚀 feat: adicionar controlador e rota para análise de código

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-exports.home = (req, res) => {
-    res.send('Welcome to CodeGuardian!');
-=======
 exports.analyzeCode = (req, res) => {
     // Lógica para análise de código virá aqui
     res.send('Análise de Código em Progresso');
->>>>>>> 5104cc8 (🚀 feat: adicionar configuração básica do ESLint)
   };
   
