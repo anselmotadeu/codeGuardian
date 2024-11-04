03d7b4303566a1cd8e973ed7b3cd1a7878b37a80
anselmotadeu <anselmotadeu@outlook.com>
2024-11-04 14:10:32 -0300

    git push origin main 🚀 feat: adicionar configuração básica do ESLint

--- src/app.js
+++ src/app.js
@@ -10,7 +10,13 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(cors());
 
+<<<<<<< HEAD
 app.use('/', routes);
+=======
+app.get('/', (req, res) => {
+  res.send('Hello CodeGuardian');
+});
+>>>>>>> 5104cc8 (🚀 feat: adicionar configuração básica do ESLint)
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
+<<<<<<< HEAD
 const controller = require('../controllers/controller');
 
 router.get('/', controller.home);
+=======
+const codeAnalysisController = require('../controllers/codeAnalysisController');
+
+router.post('/analyze', codeAnalysisController.analyzeCode);
+>>>>>>> 5104cc8 (🚀 feat: adicionar configuração básica do ESLint)
 
 module.exports = router;
