eed4f88c919371d3e640f471689020e87c34f441
anselmotadeu <anselmotadeu@outlook.com>
2024-11-04 14:46:54 -0300

    🚀 feat: adicionar análise de código para HTML e CSS

--- src/controllers/codeAnalysisController.js
+++ src/controllers/codeAnalysisController.js
@@ -1,5 +1,7 @@
 const { ESLint } = require('eslint');
 const { exec } = require('child_process');
+const HTMLHint = require('htmlhint').HTMLHint;
+const stylelint = require('stylelint');
 
 exports.analyzeCode = async (req, res) => {
   const code = req.body.code;
@@ -20,6 +22,12 @@ exports.analyzeCode = async (req, res) => {
           res.json({ analysis: stdout });
         }
       });
+    } else if (language === 'html') {
+      const results = HTMLHint.verify(code);
+      res.json({ analysis: results });
+    } else if (language === 'css') {
+      const results = await stylelint.lint({ code });
+      res.json({ analysis: results.output });
     } else {
       res.status(400).json({ error: 'Unsupported language' });
     }
