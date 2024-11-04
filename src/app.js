ec1ea6ee916678c7ea244aa39db14cdcb54e0acb
anselmotadeu <anselmotadeu@outlook.com>
2024-11-04 07:51:25 -0300

    🚀 feat: configure middleware and initial setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
 const port = 3000;
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello CodeGuardian!');
